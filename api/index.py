e700bc75d6c87a3201a74f1015342b3d1337a958
sodikinnaa <80803209+sodikinnaa@users.noreply.github.com>
2025-07-19 13:14:02 +0000

    push data

--- api/index.py
+++ api/index.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask import render_template
 
+class Config:
+    def gemini_core(self):
+        return 'gemini_core'
+    
+    
+gemini = Config()
+print(gemini.gemini_core())
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +17,10 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
